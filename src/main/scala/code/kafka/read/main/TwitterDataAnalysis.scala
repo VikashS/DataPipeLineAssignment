c8fd99c58c5c8226438987cc3fd0ff9911306e67
Vikash Singh <glovesdown@gmail.com>
2019-07-20 00:09:45 +0530

    First Commit

--- src/main/scala/code/kafka/read/main/TwitterDataAnalysis.scala
+++ src/main/scala/code/kafka/read/main/TwitterDataAnalysis.scala
@@ -1,44 +1,53 @@
 package code.kafka.read.main
 
 import code.kafka.read.main.TwitterSchema.RawTwitterData
+import scala.concurrent.duration._
 import kafka.serializer.StringDecoder
-import org.apache.log4j.Logger
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import com.datastax.spark.connector.cql.CassandraConnector
 
 object TwitterDataAnalysis {
 
-  val localLogger = Logger.getLogger("TwitterDataAnalysis")
   def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
     val sparkConf = new SparkConf().setAppName("TwitterDataAnalysis")
     sparkConf.setIfMissing("spark.master", "local[5]")
     sparkConf.setIfMissing("spark.cassandra.connection.host", "127.0.0.1")
     val ssc = new StreamingContext(sparkConf, Seconds(10))
     val kafkaTopicRaw = "mytopics"
     val kafkaBroker = "127.0.01:9092"
-    //cassandra details
-    val cassandraKeyspace = "isd_weather_data"
-    val cassandraTableRaw = "raw_weather_data"
-    val cassandraTableDailyPrecip = "daily_aggregate_precip"
+
 
     val topics: Set[String] = kafkaTopicRaw.split(",").map(_.trim).toSet
     val kafkaParams = Map[String, String]("metadata.broker.list" -> kafkaBroker)
-    val rawTwitterStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
-    val parsedTwitterStream: DStream[RawTwitterData] = ingestStream(rawTwitterStream)
+    val rawTwitterStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics).map(_._2)
 
-    parsedTwitterStream.print
+    //val parsedWeatherStream: DStream[RawTwitterData] = ingestStream(rawTwitterStream)
+
+
+    val hashTags = rawTwitterStream.flatMap(_.split(",")).filter(_.contains("@domAAdom"))
+    val topTenPopularTwiite = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+      .map { case (topic, count) => (count, topic) }
+      .transform(_.sortByKey(false))
+
+    val counyVal = topTenPopularTwiite.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular channel in last 10 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })
+
+
+
+    //ssc.checkpoint("addd the hdfs path from cloudera vm  hdfs://ip:8020/test")
+    rawTwitterStream.print
     ssc.start()
     ssc.awaitTermination()
     ssc.stop()
-  }
 
-
-  def ingestStream(rawTwitterStream: InputDStream[(String, String)]): DStream[RawTwitterData] = {
-    val parsedWeatherStream = rawTwitterStream.map(_._2.split(","))
-      .map(RawTwitterData(_))
-    parsedWeatherStream
   }
 
 }
--- src/main/scala/code/kafka/read/main/TwitterSchema.scala
+++ src/main/scala/code/kafka/read/main/TwitterSchema.scala
@@ -5,62 +5,31 @@ object TwitterSchema {
   sealed trait TwitterSchema extends Serializable
 
   case class TwitterData(
-                             id: String,
-                             name: String,
-                             countryCode: String,
-                             callSign: String,
-                             lat: Double,
-                             long: Double,
-                             elevation: Double) extends TwitterSchema
+                          id: String,
+                          name: String,
+                          countryCode: String,
+                          callSign: String,
+                          lat: Double,
+                          long: Double,
+                          elevation: Double) extends TwitterSchema
 
   case class RawTwitterData(
                              wsid: String,
-                             year: Int,
-                             month: Int,
-                             day: Int,
-                             hour: Int,
-                             temperature: Double,
-                             dewpoint: Double,
-                             pressure: Double,
-                             windDirection: Int,
-                             windSpeed: Double,
-                             skyCondition: Int,
-                             skyConditionText: String,
-                             oneHourPrecip: Double,
-                             sixHourPrecip: Double) extends TwitterSchema
-
+                             count: Int,
+                             topic: String) extends TwitterSchema
 
   object RawTwitterData {
     def apply(array: Array[String]): RawTwitterData = {
       RawTwitterData(
         wsid = array(0),
-        year = array(1).toInt,
-        month = array(2).toInt,
-        day = array(3).toInt,
-        hour = array(4).toInt,
-        temperature = array(5).toDouble,
-        dewpoint = array(6).toDouble,
-        pressure = array(7).toDouble,
-        windDirection = array(8).toInt,
-        windSpeed = array(9).toDouble,
-        skyCondition = array(10).toInt,
-        skyConditionText = array(11),
-        oneHourPrecip = array(11).toDouble,
-        sixHourPrecip = Option(array(12).toDouble).getOrElse(0))
+        count = array(1).toInt,
+        topic = array(2)
+      )
     }
   }
 
   trait TwitterAggregate extends TwitterSchema with Serializable {
     def wsid: String
   }
-
-  trait Precipitation extends TwitterAggregate
-
-  case class DailyAnalysis(wsid: String,
-                                year: Int,
-                                month: Int,
-                                day: Int,
-                                precipitation: Double) extends Precipitation
-
 
 }
